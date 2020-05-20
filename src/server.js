596d30ffddf785f8ced60dc1c5ad8b04ad18d983
Takafumi Tamura <taktamur@gmail.com>
2020-05-20 20:06:39 +0900

    plainTextで今月分返却できるようになった

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
+const _ = require("./lib");
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  var today = new Date();
+  // if (program.args.length > 0) {
+  //   today = new Date(program.args[0]);
+  // }
+  const days = _.lines(today.getFullYear(), today.getMonth() + 1);
+  res.header("Content-Type", "text/plain;charset=utf-8");
+  res.end(days.join("\n"));
+});
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
